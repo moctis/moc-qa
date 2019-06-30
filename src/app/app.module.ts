c6bbe9fb83dfe703e02b5ad9c04eea122400d74a
moctis <sitthidate@moctis.com>
2019-06-30 23:33:08 +0700

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TestComponent } from './test.component';
 import { AppRoutingModule } from './app.routing.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, NgbModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, NgbModule, Ng4LoadingSpinnerModule.forRoot() ],
   declarations: [ AppComponent, FictionlogComponent, HelloComponent, TestComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/fictionlog.component.ts
+++ src/app/fictionlog.component.ts
@@ -1,43 +1,45 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'fictionlog',
   templateUrl: './fictionlog.component.html',
-  styleUrls: [ './fictionlog.css' ],
+  styleUrls: ['./fictionlog.css'],
 })
-export class FictionlogComponent  {
-  @Input() name: string; 
-  token:string = "";
+export class FictionlogComponent {
+  @Input() name: string;
+  token: string = "";
   chapterId: string = "5c1b1775a61d8b3255ac24da";
   bookId: string = "5b73fe93c7d65a2966068482";
-  data:any;
-  book:any = {};
-  current:any;
-  chapterIndex:any = 0;
-  eventText:string;
-  bookInfo:any;
-  blocks:any[] = [];
+  data: any;
+  book: any = {};
+  current: any;
+  chapterIndex: any = 0;
+  eventText: string;
+  bookInfo: any;
+  blocks: any[] = [];
   private sub: any;
   errorText: any;
+  time = 30000;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
-     this.current = { chapterIndex : 0};
+  constructor(private http: HttpClient, private route: ActivatedRoute, private spinnerService: Ng4LoadingSpinnerService) {
+    this.current = { chapterIndex: 0 };
   }
 
   ngOnInit() {
-      var t = localStorage.getItem("access");
-      if (t != undefined)
-        this.token ="SAVED!";
+    var t = localStorage.getItem("access");
+    if (t != undefined)
+      this.token = "SAVED!";
 
-      this.sub = this.route.params.subscribe(params => {
-        this.bookId = params['id']; // (+) converts string 'id' to a number
-          this.current = { chapterIndex : 0};
-          this.load();
-        // In a real app: dispatch action to load the details here.
-      });
-   
+    this.sub = this.route.params.subscribe(params => {
+      this.bookId = params['id']; // (+) converts string 'id' to a number
+      this.current = { chapterIndex: 0 };
+      this.load();
+      // In a real app: dispatch action to load the details here.
+    });
+
   }
 
   ngOnDestroy() {
@@ -48,7 +50,7 @@ export class FictionlogComponent  {
     this.blocks = [];
     try {
       var libraries = JSON.parse(localStorage.getItem("libraries"));
-      this.bookInfo = libraries.find(p=>p._id==this.bookId);      
+      this.bookInfo = libraries.find(p => p._id == this.bookId);
 
       this.current = JSON.parse(localStorage.getItem("current." + this.bookId)) || {};
       this.book = JSON.parse(localStorage.getItem("bookId." + this.bookId));
@@ -56,21 +58,21 @@ export class FictionlogComponent  {
       this.swipe(0);
       // this.chapterId = this.book.chapterList.chapters[this.current.chapterIndex]._id;
       // this.data = JSON.parse(localStorage.getItem("chapterId." + this.chapterId));
-    } catch(e) { this.errorText = e;}
-    
+    } catch (e) { this.errorText = e; }
+
   }
-  setToken () {
+  setToken() {
     localStorage.setItem("access", this.token);
   }
 
   loadChapter() {
     try {
-    this.chapterId = this.book.chapterList.chapters[this.current.chapterIndex]._id;
-    this.data = JSON.parse(localStorage.getItem("chapterId." + this.chapterId));
-    this.splitBlock();
-    } catch(e) { this.errorText = e;}
+      this.chapterId = this.book.chapterList.chapters[this.current.chapterIndex]._id;
+      this.data = JSON.parse(localStorage.getItem("chapterId." + this.chapterId));
+      this.splitBlock();
+    } catch (e) { this.errorText = e; }
   }
-  
+
   splitBlock() {
     var x = this.data || {};
     x = x.chapter || {};
@@ -94,11 +96,11 @@ export class FictionlogComponent  {
     var count = 0;
     try {
       count = this.book.chapterList.chapters.length;
-    }catch{ count = 1;}
+    } catch{ count = 1; }
 
     this.current.chapterIndex = this.current.chapterIndex || 0;
     this.current.chapterIndex += diff;
-    this.current.chapterIndex = (this.current.chapterIndex+count) % count;
+    this.current.chapterIndex = (this.current.chapterIndex + count) % count;
     localStorage.setItem("current." + this.bookId, JSON.stringify(this.current));
     this.loadChapter();
   }
@@ -106,67 +108,72 @@ export class FictionlogComponent  {
   getBookDetail() {
     const access = localStorage.getItem("access");
     const options = {
-    headers: new HttpHeaders({
-      'Authorization': access
-    }),
+      headers: new HttpHeaders({
+        'Authorization': access
+      }),
     };
-    var req = {"operationName":null,"variables":{"bookId":this.bookId},"query":"query ($bookId: ID!) {\n  book(bookId: $bookId) {\n    ...BookFragment\n    canSellBundle\n    isWriter\n    status\n    hasMatureContent\n    description\n    viewsCount\n    inLibrariesCount\n    hasPaidChapter\n    userReviewSummary {\n      label\n      color\n      __typename\n    }\n    userReviewsCount {\n      positive\n      negative\n      total\n      __typename\n    }\n    bundlePrice {\n      goldCoin\n      __typename\n    }\n    totalPrice {\n      goldCoin\n      __typename\n    }\n    chaptersCount {\n      public\n      draft\n      total\n      __typename\n    }\n    neighbors {\n      _id\n      coverImage\n      title\n      completed\n      bundlePriceTierDetail {\n        _id\n        discount\n        __typename\n      }\n      user {\n        _id\n        displayName\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment BookFragment on Book {\n  _id\n  coverImage\n  bannerImage\n  bundlePurchased\n  title\n  contentRawState\n  addedToLibrary\n  hashtags\n  hasMatureContent\n  placeholderBackgroundColor\n  description\n  viewsCount\n  completed\n  publishedAt\n  status\n  latestChapterPublishedAt\n  latestViewedChapter\n  bundlePriceTierDetail {\n    _id\n    discount\n    __typename\n  }\n  category {\n    _id\n    name\n    __typename\n  }\n  user {\n    _id\n    username\n    displayName\n    __typename\n  }\n  __typename\n}\n"};
+    var req = { "operationName": null, "variables": { "bookId": this.bookId }, "query": "query ($bookId: ID!) {\n  book(bookId: $bookId) {\n    ...BookFragment\n    canSellBundle\n    isWriter\n    status\n    hasMatureContent\n    description\n    viewsCount\n    inLibrariesCount\n    hasPaidChapter\n    userReviewSummary {\n      label\n      color\n      __typename\n    }\n    userReviewsCount {\n      positive\n      negative\n      total\n      __typename\n    }\n    bundlePrice {\n      goldCoin\n      __typename\n    }\n    totalPrice {\n      goldCoin\n      __typename\n    }\n    chaptersCount {\n      public\n      draft\n      total\n      __typename\n    }\n    neighbors {\n      _id\n      coverImage\n      title\n      completed\n      bundlePriceTierDetail {\n        _id\n        discount\n        __typename\n      }\n      user {\n        _id\n        displayName\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment BookFragment on Book {\n  _id\n  coverImage\n  bannerImage\n  bundlePurchased\n  title\n  contentRawState\n  addedToLibrary\n  hashtags\n  hasMatureContent\n  placeholderBackgroundColor\n  description\n  viewsCount\n  completed\n  publishedAt\n  status\n  latestChapterPublishedAt\n  latestViewedChapter\n  bundlePriceTierDetail {\n    _id\n    discount\n    __typename\n  }\n  category {\n    _id\n    name\n    __typename\n  }\n  user {\n    _id\n    username\n    displayName\n    __typename\n  }\n  __typename\n}\n" };
 
   }
   getBook() {
     const access = localStorage.getItem("access");
     const options = {
-    headers: new HttpHeaders({
-      'Authorization': access
-    }),
+      headers: new HttpHeaders({
+        'Authorization': access
+      }),
     };
 
-    var req = {"operationName":null,"variables":{"bookId":this.bookId},"query":"query ($bookId: ID!, $filter: ChapterListFilter) {\n  chapterList(bookId: $bookId, filter: $filter) {\n    chapters {\n      _id\n      title\n      viewsCount\n      chapterCommentsCount\n      isPurchaseRequired\n      publishedAt\n      status\n      book {\n        _id\n        __typename\n      }\n      price {\n        type\n        silverCoin\n        goldCoin\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"};
+    var req = { "operationName": null, "variables": { "bookId": this.bookId }, "query": "query ($bookId: ID!, $filter: ChapterListFilter) {\n  chapterList(bookId: $bookId, filter: $filter) {\n    chapters {\n      _id\n      title\n      viewsCount\n      chapterCommentsCount\n      isPurchaseRequired\n      publishedAt\n      status\n      book {\n        _id\n        __typename\n      }\n      price {\n        type\n        silverCoin\n        goldCoin\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n" };
 
-      this.http.post('https://api.k8s.fictionlog.co/graphql', req, options).subscribe(data => {
+    this.spinnerService.show();
+    setTimeout(() => this.spinnerService.hide(), this.time);
+    this.http.post('https://api.k8s.fictionlog.co/graphql', req, options).subscribe(data => {
+      this.spinnerService.hide();
       this.book = data['data'];
 
-      localStorage.setItem("bookId."+this.bookId, JSON.stringify(this.book));
+      localStorage.setItem("bookId." + this.bookId, JSON.stringify(this.book));
       this.current.bookId = this.bookId;
       localStorage.setItem("current." + this.bookId, JSON.stringify(this.current));
       this.swipe(0);
       //{{book?.chapterList?.chapters[current.chapterIndex]?.title}}
-    });  
+    });
   }
 
   getChapter() {
     const access = localStorage.getItem("access");
     const options = {
-    headers: new HttpHeaders({
-      'Authorization': access
-    }),
+      headers: new HttpHeaders({
+        'Authorization': access
+      }),
     };
     this.chapterId = this.book.chapterList.chapters[this.current.chapterIndex]._id;
 
 
-    var req = {"operationName":null,"variables":{"chapterId": this.chapterId},"query":"query ($chapterId: ID!) {\n  chapter(chapterId: $chapterId) {\n    ...ChapterFragment\n    priceId\n    userId\n    viewsCount\n    chapterCommentsCount\n    book {\n      _id\n      title\n      description\n      coverImage\n      addedToLibrary\n      hasPaidChapter\n      bundlePurchased\n      bundlePrice {\n        goldCoin\n        __typename\n      }\n      totalPrice {\n        goldCoin\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ChapterFragment on Chapter {\n  _id\n  title\n  contentRawState\n  isWriter\n  status\n  publishedAt\n  isPurchaseRequired\n  priceId\n  price {\n    type\n    goldCoin\n    silverCoin\n    __typename\n  }\n  user {\n    _id\n    displayName\n    username\n    __typename\n  }\n  nextChapter {\n    _id\n    title\n    price {\n      type\n      goldCoin\n      silverCoin\n      __typename\n    }\n    status\n    purchased\n    bookId\n    __typename\n  }\n  editChapter {\n    _id\n    title\n    contentRawState\n    status\n    rejectNote\n    __typename\n  }\n  __typename\n}\n"}
-
-     this.http.post('https://api.k8s.fictionlog.co/graphql', req, options).subscribe(data => {
+    var req = { "operationName": null, "variables": { "chapterId": this.chapterId }, "query": "query ($chapterId: ID!) {\n  chapter(chapterId: $chapterId) {\n    ...ChapterFragment\n    priceId\n    userId\n    viewsCount\n    chapterCommentsCount\n    book {\n      _id\n      title\n      description\n      coverImage\n      addedToLibrary\n      hasPaidChapter\n      bundlePurchased\n      bundlePrice {\n        goldCoin\n        __typename\n      }\n      totalPrice {\n        goldCoin\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ChapterFragment on Chapter {\n  _id\n  title\n  contentRawState\n  isWriter\n  status\n  publishedAt\n  isPurchaseRequired\n  priceId\n  price {\n    type\n    goldCoin\n    silverCoin\n    __typename\n  }\n  user {\n    _id\n    displayName\n    username\n    __typename\n  }\n  nextChapter {\n    _id\n    title\n    price {\n      type\n      goldCoin\n      silverCoin\n      __typename\n    }\n    status\n    purchased\n    bookId\n    __typename\n  }\n  editChapter {\n    _id\n    title\n    contentRawState\n    status\n    rejectNote\n    __typename\n  }\n  __typename\n}\n" }
+    this.spinnerService.show();
+    setTimeout(() => this.spinnerService.hide(), this.time);
+    this.http.post('https://api.k8s.fictionlog.co/graphql', req, options).subscribe(data => {
+      this.spinnerService.hide();
       this.setChapter(data['data'])
-    });  
+    });
   }
 
   setChapter(data) {
     //console.log('setChapter', data);
-      this.data = data;
-      // localStorage.setItem("chapterId."+this.chapterId, JSON.stringify(this.data));
-      this.current.chapterId = this.chapterId;
-      localStorage.setItem("current." + this.bookId, JSON.stringify(this.current));
+    this.data = data;
+    // localStorage.setItem("chapterId."+this.chapterId, JSON.stringify(this.data));
+    this.current.chapterId = this.chapterId;
+    localStorage.setItem("current." + this.bookId, JSON.stringify(this.current));
 
-      this.book.chapterList.chapters[this.current.chapterIndex].isPurchaseRequired =           this.data.chapter.isPurchaseRequired;
-      localStorage.setItem("bookId."+this.bookId, JSON.stringify(this.book));
-      this.splitBlock();
+    this.book.chapterList.chapters[this.current.chapterIndex].isPurchaseRequired = this.data.chapter.isPurchaseRequired;
+    localStorage.setItem("bookId." + this.bookId, JSON.stringify(this.book));
+    this.splitBlock();
 
   }
 
   purchaseChapter() {
 
-    var chapter =this.book.chapterList.chapters[this.current.chapterIndex];
+    var chapter = this.book.chapterList.chapters[this.current.chapterIndex];
     if (chapter.isPurchaseRequired == false)
       return;
     this.chapterId = chapter._id;
@@ -179,7 +186,7 @@ export class FictionlogComponent  {
       amount = price.silverCoin;
     } else {
       coinType = "goldCoin";
-      amount = price.goldCoin;  
+      amount = price.goldCoin;
     }
 
     const access = localStorage.getItem("access");
@@ -188,24 +195,26 @@ export class FictionlogComponent  {
         'Authorization': access
       }),
     };
- 
-    var req = {"operationName":null,"variables":{"chapterId":this.chapterId,"input":{"coinType":coinType,"amount":amount}},"query":"mutation ($chapterId: ID!, $input: PurchaseInput!) {\n  purchaseChapter(chapterId: $chapterId, input: $input) {\n    ...ChapterFragment\n    viewsCount\n    chapterCommentsCount\n    book {\n      _id\n      title\n      coverImage\n      addedToLibrary\n      hasPaidChapter\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ChapterFragment on Chapter {\n  _id\n  title\n  contentRawState\n  isWriter\n  status\n  publishedAt\n  isPurchaseRequired\n  priceId\n  price {\n    type\n    goldCoin\n    silverCoin\n    __typename\n  }\n  user {\n    _id\n    displayName\n    username\n    __typename\n  }\n  nextChapter {\n    _id\n    title\n    price {\n      type\n      goldCoin\n      silverCoin\n      __typename\n    }\n    status\n    purchased\n    bookId\n    __typename\n  }\n  editChapter {\n    _id\n    title\n    contentRawState\n    status\n    rejectNote\n    __typename\n  }\n  __typename\n}\n"}
 
+    var req = { "operationName": null, "variables": { "chapterId": this.chapterId, "input": { "coinType": coinType, "amount": amount } }, "query": "mutation ($chapterId: ID!, $input: PurchaseInput!) {\n  purchaseChapter(chapterId: $chapterId, input: $input) {\n    ...ChapterFragment\n    viewsCount\n    chapterCommentsCount\n    book {\n      _id\n      title\n      coverImage\n      addedToLibrary\n      hasPaidChapter\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ChapterFragment on Chapter {\n  _id\n  title\n  contentRawState\n  isWriter\n  status\n  publishedAt\n  isPurchaseRequired\n  priceId\n  price {\n    type\n    goldCoin\n    silverCoin\n    __typename\n  }\n  user {\n    _id\n    displayName\n    username\n    __typename\n  }\n  nextChapter {\n    _id\n    title\n    price {\n      type\n      goldCoin\n      silverCoin\n      __typename\n    }\n    status\n    purchased\n    bookId\n    __typename\n  }\n  editChapter {\n    _id\n    title\n    contentRawState\n    status\n    rejectNote\n    __typename\n  }\n  __typename\n}\n" }
+    this.spinnerService.show();
+    setTimeout(() => this.spinnerService.hide(), this.time);
     this.http.post('https://api.k8s.fictionlog.co/graphql', req, options).subscribe(data => {
+      this.spinnerService.hide();
       //console.log('done');
       //console.log(data);
       chapter.silverCoin = null;
       chapter.goldCoin = null
       chapter.isPurchaseRequired = false;
-      this.setChapter({'chapter':data['data']['purchaseChapter']})
-    }); 
+      this.setChapter({ 'chapter': data['data']['purchaseChapter'] })
+    });
   }
 
-  
-  onSwipe(evt) {
-      const x = Math.abs(evt.deltaX) > 40 ? (evt.deltaX > 0 ? 'right' : 'left'):'';
-      const y = Math.abs(evt.deltaY) > 40 ? (evt.deltaY > 0 ? 'down' : 'up') : '';
 
-      this.eventText += `${x} ${y}<br/>`;
+  onSwipe(evt) {
+    const x = Math.abs(evt.deltaX) > 40 ? (evt.deltaX > 0 ? 'right' : 'left') : '';
+    const y = Math.abs(evt.deltaY) > 40 ? (evt.deltaY > 0 ? 'down' : 'up') : '';
+
+    this.eventText += `${x} ${y}<br/>`;
   }
 }
